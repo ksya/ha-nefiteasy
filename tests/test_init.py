a9183fe2bdeac3454bf8abc09136f8b81217ba0a
Rob Bierbooms <mail@robbierbooms.nl>
2021-12-29 21:30:40 +0100

    Fix dependencies

--- tests/test_init.py
+++ tests/test_init.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 
-from pytest_homeassistant_custom_component.common import MockConfigEntry, load_fixture import MockConfigEntry
+from pytest_homeassistant_custom_component.common import MockConfigEntry, load_fixture
 from .conftest import ClientMock
 
 
